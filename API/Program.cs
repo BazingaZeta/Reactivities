1e4e8185e8ffabfd76d636182350573115370e7e
BazingaZeta <edoardo.mike.trotta@gmail.com>
2020-09-14 16:03:33 +0100

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            
+            //What goes in in the using method will be cleaned up after it's completed
+            //This will run every time we run the application and will use a database from a 
+            // migration, or will create a new one.
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+
+                try {
+                    var context = services.GetRequiredService<DataContext>(); 
+                    context.Database.Migrate();
+               }
+               catch (Exception ex)
+               {
+                   var logger = services.GetRequiredService<ILogger<Program>>();
+                   logger.LogError(ex, "An error occurred during migration");
+               }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
