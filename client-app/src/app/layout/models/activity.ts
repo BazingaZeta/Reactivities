0bb0408701338beb7dfc910dfdd519b30509f052
BazingaZeta <edoardo.mike.trotta@gmail.com>
2020-09-18 19:23:46 +0100

    End of section 5

---
+++ client-app/src/app/layout/models/activity.ts
@@ -0,0 +1,11 @@
+import { StringifyOptions } from "querystring";
+
+export interface IActivity {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  date: string;
+  city: string;
+  venue: string;
+}
