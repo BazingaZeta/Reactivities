e2cc7eda7220b25c6653a310c858806f5b84a0e7
BazingaZeta <edoardo.mike.trotta@gmail.com>
2020-09-21 12:48:21 +0100

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -16,8 +16,23 @@ class ActivityStore {
   target = "";
 
   get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+    return Object.entries(
+      sortedActivities.reduce((activities, activity) => {
+        const date = activity.date.split("T")[0];
+        activities[date] = activities[date]
+          ? [...activities[date], activity]
+          : [activity];
+        return activities;
+      }, {} as { [key: string]: IActivity[] })
     );
   }
 
@@ -53,7 +68,7 @@ class ActivityStore {
           this.loadingInitial = false;
         });
       } catch (error) {
-        runInAction("get activiti error", () => {
+        runInAction("get activity error", () => {
           this.loadingInitial = false;
         });
         console.log(error);
